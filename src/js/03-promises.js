6833f08a4b2aae90eacdb1d9cfcd2abf89b344f0
Anna Datsiuk <anna.datsiuk94@gmail.com>
2022-08-21 23:43:39 +0300

    add task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const Elementform = document.querySelector('.form');
+Elementform.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  let delay = Number(event.target.elements.delay.value);
+  let step = Number(event.target.elements.step.value);
+  let amount = Number(event.target.elements.amount.value);
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+         console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
+  }
+}
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return promise;
 }
