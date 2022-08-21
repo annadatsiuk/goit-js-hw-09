3ec1b41f74e770bb42a2976a4d54481f3169dfe1
Anna Datsiuk <anna.datsiuk94@gmail.com>
2022-08-21 23:28:38 +0300

    add 1st task and css

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+  
+const body = document.querySelector("body");
+const btnStart = document.querySelector("button[data-start]");
+const btnStop = document.querySelector("button[data-stop]");
+
+let timerId = null;
+
+btnStop.disabled = true;
+
+btnStart.addEventListener("click",onStart);
+btnStop.addEventListener("click", onStop);
+
+function onStart () {
+    btnStart.disabled = true,
+    btnStop.disabled = false,
+    timerId = setInterval(() => {
+    body.style.background = getRandomHexColor()
+    },1000)
+};
+
+function onStop(){
+    if(timerId) {
+        clearInterval(timerId);
+    btnStart.disabled = false
+    btnStop.disabled = true
+    }
+    return
+};
 
